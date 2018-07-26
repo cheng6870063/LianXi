4378b0f80069f8c72d3a8371e652c585c84c004a
cheng6870063 <1291324977@qq.com>
2018-07-26 09:39:24 +0800

    新增

--- springmvc/src/main/java/com/cxx/controller/HelloController.java
+++ springmvc/src/main/java/com/cxx/controller/HelloController.java
@@ -1,18 +1,23 @@
 package com.cxx.controller;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.apache.log4j.Logger;
 
 @Controller
 @RequestMapping(value = "/hello", method = RequestMethod.GET)
 public class HelloController {
-
+    private static Logger logger = Logger.getLogger(HelloController.class);
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("msg", "Spring MVC Hello World");
         model.addAttribute("name", "yuntao");
+        logger.info("访问首页");
+        logger.debug("ccccccccc");
         return "hello";
     }
 }
--- springmvc/src/main/java/com/cxx/controller/HelloReaderController.java
+++ springmvc/src/main/java/com/cxx/controller/HelloReaderController.java
@@ -1,5 +1,7 @@
 package com.cxx.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,11 +12,16 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping(value="/hellos")
 public class HelloReaderController {
 
+    // 添加slf4j日志实例对象
+    final static Logger logger = LoggerFactory.getLogger(HelloReaderController.class);
+
     @RequestMapping(value = "/hellos", method = GET)
     public ModelAndView sayHello() {
         ModelAndView mv = new ModelAndView();
         mv.addObject("message", "Hello Reader!");
         mv.setViewName("helloReader");
+        // 输出日志
+        logger.info("测试：{}", "输出日志");
         return mv;
     }
 }
