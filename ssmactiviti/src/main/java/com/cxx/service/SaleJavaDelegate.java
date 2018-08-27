7efe90acd84cf687d8b970f364a9036d71bc6ca7
cheng6870063 <1291324977@qq.com>
2018-08-27 11:19:01 +0800

    新增

--- ssmactiviti/src/main/java/com/cxx/service/SaleJavaDelegate.java
+++ ssmactiviti/src/main/java/com/cxx/service/SaleJavaDelegate.java
@@ -3,10 +3,14 @@ package com.cxx.service;
 import com.cxx.bean.Sale;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.apache.log4j.Logger;
 
 import java.util.Collection;
 
 public class SaleJavaDelegate implements JavaDelegate {
+
+    private static final Logger logger = Logger.getLogger(SaleJavaDelegate.class);
+
 
     public void execute(DelegateExecution execution) throws Exception {
         Collection sales = (Collection) execution.getVariable("saleResults");
@@ -15,7 +19,8 @@ public class SaleJavaDelegate implements JavaDelegate {
             Sale sale  = (Sale) obj;
             System.out.println("销售单："+sale.getSaleCode()+" ,原件："+sale.getTotal()+
                     " ,优惠后："+sale.getDiscountTotal()+" ,折扣:"+sale.getDiscount());
-
+            logger.info("销售单："+sale.getSaleCode()+" ,原件："+sale.getTotal()+
+                    " ,优惠后："+sale.getDiscountTotal()+" ,折扣:"+sale.getDiscount());
         }
     }
 }
